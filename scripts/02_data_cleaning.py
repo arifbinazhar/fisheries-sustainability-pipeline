c2fb6318a85faf3db6e3b8f0bc6722fdfdb77144
Arif Bin Azhar <arifbinazhar03@gmail.com>
2026-02-19 13:18:39 +0530

    Update 02_data_cleaning.py

--- scripts/02_data_cleaning.py
+++ scripts/02_data_cleaning.py
@@ -1,20 +1,20 @@
-import pandas as pd
-import yaml
+# import pandas as pd
+# import yaml
 
-with open("config/config.yaml") as f:
-    config = yaml.safe_load(f)
+# with open("config/config.yaml") as f:
+#     config = yaml.safe_load(f)
 
-raw_path = config["data"]["raw_path"]
-cleaned_path = config["data"]["cleaned_path"]
+# raw_path = config["data"]["raw_path"]
+# cleaned_path = config["data"]["cleaned_path"]
 
-df = pd.read_csv(raw_path)
+# df = pd.read_csv(raw_path)
 
-df.columns = df.columns.str.lower().str.strip()
+# df.columns = df.columns.str.lower().str.strip()
 
-df = df.drop_duplicates()
+# df = df.drop_duplicates()
 
-df = df.dropna()
+# df = df.dropna()
 
-df.to_csv(cleaned_path, index=False)
+# df.to_csv(cleaned_path, index=False)
 
-print("Cleaned data saved.")
+# print("Cleaned data saved.")
